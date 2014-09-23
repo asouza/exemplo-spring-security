922f0080a621617f1ba359dae51bd4d4d5b291ab
asouza <alots.ssa@gmail.com>
2014-09-23 16:35:37 -0300

    primeiro controller sendo acessado

--- src/main/java/br/com/caelum/lojasupersegura/WebConfig.java
+++ src/main/java/br/com/caelum/lojasupersegura/WebConfig.java
@@ -1,11 +1,22 @@
 package br.com.caelum.lojasupersegura;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages={"br.com.caelum.lojasupersegura"})
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+	  InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	  resolver.setPrefix("/WEB-INF/views/");
+	  resolver.setSuffix(".jsp");	 
+	  return resolver;
+	}
 }
